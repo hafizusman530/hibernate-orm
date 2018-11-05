becbd001cbc9d47d3ecf7fb3e110008c776a7c7f
Muhammad Usman <muhammad.usman@tacton.com>
2018-11-05 10:17:48 +0500

    Add hibernateUtil class for getting sessionFactory instance

--- src/main/java/com/usman/practice/hibernate/util/HibernateUtil.java
+++ src/main/java/com/usman/practice/hibernate/util/HibernateUtil.java
@@ -1,4 +1,24 @@
 package com.usman.practice.hibernate.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    //    Session uses SessionFactory, which contains database configuration. if your app have multiple DBs you need
+//    to have configure multiple sessionFactory instances for each database. SessionFactory is thread safe.
+    private static SessionFactory sessionFactory;
+
+    static {
+        loadSessionFactory();
+    }
+
+    private static void loadSessionFactory() {
+        sessionFactory = new Configuration()
+                .configure().buildSessionFactory();
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
 }
